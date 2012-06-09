89a62d703a839d1ce2452a84dd76375710eab719
William Swanson <swansontec@gmail.com>
2012-06-08 19:42:42 -0700

    Get the IVsTextView adapter object

--- Plugin/PluginFactory.cs
+++ Plugin/PluginFactory.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
 
 namespace EditorConfig.VisualStudio
@@ -10,19 +12,26 @@ namespace EditorConfig.VisualStudio
     /// to each one. Visual Studio to magically instantiates this class and
     /// calls it at the correct time, thanks to the fancy metadata.
     /// </summary>
-    [Export(typeof(IWpfTextViewCreationListener))]
+    [Export(typeof(IVsTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
-    internal sealed class PluginFactory : IWpfTextViewCreationListener
+    internal sealed class PluginFactory : IVsTextViewCreationListener
     {
+        [Import]
+        IVsEditorAdaptersFactoryService adapterFactory = null;
+
         [Import]
         ITextDocumentFactoryService docFactory = null;
 
         /// <summary>
         /// Creates a plugin instance when a new text editor is opened
         /// </summary>
-        public void TextViewCreated(IWpfTextView view)
+        public void VsTextViewCreated(IVsTextView adapter)
         {
+            IWpfTextView view = adapterFactory.GetWpfTextView(adapter);
+            if (view == null)
+                return;
+
             ITextDocument document;
             if (!docFactory.TryGetTextDocument(view.TextDataModel.DocumentBuffer, out document))
                 return;
