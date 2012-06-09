9179a84932db8f9a6973f3d3e1bbfb3672cca64b
William Swanson <swansontec@gmail.com>
2012-06-08 20:46:14 -0700

    Handle filename changes

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -11,8 +11,27 @@ namespace EditorConfig.VisualStudio
     {
         public Plugin(IWpfTextView view, ITextDocument document)
         {
+            document.FileActionOccurred += FileActionOccurred;
+
+            LoadSettings(document.FilePath);
+        }
+
+        /// <summary>
+        /// Reloads the settings when the filename changes
+        /// </summary>
+        void FileActionOccurred(object sender, TextDocumentFileActionEventArgs e)
+        {
+            if ((e.FileActionType & FileActionTypes.DocumentRenamed) != 0)
+                LoadSettings(e.FilePath);
+        }
+
+        /// <summary>
+        /// Loads the settings for the given file path
+        /// </summary>
+        private void LoadSettings(string path)
+        {
             using (var log = new System.IO.StreamWriter(System.IO.Path.GetTempPath() + "editorconfig.log"))
-                log.Write("Plugin object created for document " + document.FilePath);
+                log.Write("Load settings for file " + path);
         }
     }
 }
