b09ff34bba7dd8919ab8774caa315f1a771e4ca6
William Swanson <swansontec@gmail.com>
2012-06-14 00:52:00 -0700

    Add proper exceptions to the wrapper

--- Wrapper/wrapper.cpp
+++ Wrapper/wrapper.cpp
@@ -5,6 +5,41 @@ namespace EditorConfig {
 
   typedef System::Collections::Generic::Dictionary<String^, String^> Results;
 
+  public ref class ParseException : public System::Exception
+  {
+  private:
+    String ^file;
+    int line;
+
+  public:
+    ParseException(String ^file, int line)
+    {
+      this->line = line;
+      this->file = file;
+    }
+
+    property String ^File {
+      String ^get() {
+        return this->file;
+      }
+    }
+
+    property int Line {
+      int get() {
+        return this->line;
+      }
+    }
+  };
+
+  public ref class CoreException : public System::Exception
+  {
+  public:
+    CoreException(String ^message) :
+      System::Exception(message)
+    {
+    }
+  };
+
   public ref class Core
   {
   public:
@@ -14,8 +49,11 @@ namespace EditorConfig {
       editorconfig_handle handle = editorconfig_handle_init();
 
       int rv = editorconfig_parse(name, handle);
-      if (rv)
-        throw gcnew Exception(); // Add real error messages
+      if (0 < rv) {
+        throw gcnew ParseException(UTF8ToString(editorconfig_handle_get_err_file(handle)), rv);
+      } else if (rv < 0) {
+        throw gcnew CoreException(UTF8ToString(editorconfig_get_error_msg(rv)));
+      }
 
       // Package the results into a .net collection datatype:
       Results ^dict = gcnew Results();
