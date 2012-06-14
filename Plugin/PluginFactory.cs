8742fd909c2c6b333aef72e0ee08676fd2714006
William Swanson <swansontec@gmail.com>
2012-06-14 02:25:45 -0700

    Simplify the DTE object creation

--- Plugin/PluginFactory.cs
+++ Plugin/PluginFactory.cs
@@ -1,12 +1,8 @@
 using System.ComponentModel.Composition;
-using System.Runtime.InteropServices;
 using EnvDTE;
-using Microsoft.VisualStudio.Editor;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
 
 namespace EditorConfig.VisualStudio
@@ -16,46 +12,29 @@ namespace EditorConfig.VisualStudio
     /// to each one. Visual Studio to magically instantiates this class and
     /// calls it at the correct time, thanks to the fancy metadata.
     /// </summary>
-    [Export(typeof(IVsTextViewCreationListener))]
+    [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
-    internal sealed class PluginFactory : IVsTextViewCreationListener
+    internal sealed class PluginFactory : IWpfTextViewCreationListener
     {
         [Import]
-        IVsEditorAdaptersFactoryService adapterFactory = null;
+        internal ITextDocumentFactoryService docFactory = null;
 
         [Import]
-        ITextDocumentFactoryService docFactory = null;
+        internal SVsServiceProvider serviceProvider = null;
 
         /// <summary>
         /// Creates a plugin instance when a new text editor is opened
         /// </summary>
-        public void VsTextViewCreated(IVsTextView adapter)
+        public void TextViewCreated(IWpfTextView view)
         {
-            IWpfTextView view = adapterFactory.GetWpfTextView(adapter);
-            if (view == null)
-                return;
-
             ITextDocument document;
             if (!docFactory.TryGetTextDocument(view.TextDataModel.DocumentBuffer, out document))
                 return;
 
-            IObjectWithSite iows = adapter as IObjectWithSite;
-            if (iows == null)
+            DTE dte = (DTE)serviceProvider.GetService(typeof(DTE));
+            if (dte == null)
                 return;
-
-            System.IntPtr p;
-            iows.GetSite(typeof(IServiceProvider).GUID, out p);
-            if (p == System.IntPtr.Zero)
-                return;
-
-            IServiceProvider isp = Marshal.GetObjectForIUnknown(p) as IServiceProvider;
-            if (isp == null)
-                return;
-
-            ServiceProvider sp = new ServiceProvider(isp);
-            DTE dte = sp.GetService(typeof(DTE)) as DTE;
-            sp.Dispose();
 
             new Plugin(view, document, dte);
         }
