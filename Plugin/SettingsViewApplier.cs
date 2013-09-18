8e7e282d148c6a006fd21a7bb5827057b6ef74c3
nulltoken <emeric.fermas@gmail.com>
2013-09-18 18:27:43 +0200

    Honor trim_trailing_whitespace setting

--- Plugin/SettingsViewApplier.cs
+++ Plugin/SettingsViewApplier.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.Text;
+﻿using System;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Editor.OptionsExtensionMethods;
 
@@ -12,6 +13,69 @@ namespace EditorConfig.VisualStudio
                 && settings.InsertFinalNewLine.Value)
             {
                 EnsureTrailingNewLine(view);
+            }
+
+            if (settings.TrimTrailingWhitespace.HasValue
+                && settings.TrimTrailingWhitespace.Value)
+            {
+                TrimTrailingWhitespace(view);
+            }
+        }
+
+        private static bool IsWhiteSpace(Char c)
+        {
+            switch (c)
+            {
+                case ' ':
+                case '\t':
+                case '\f':
+                case '\v':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static void TrimTrailingWhitespace(IWpfTextView view)
+        {
+            ITextSnapshot snapshot = view.TextSnapshot;
+            var lineCount = snapshot.LineCount;
+
+            if (lineCount == 0)
+            {
+                return;
+            }
+
+            using (var edit = snapshot.TextBuffer.CreateEdit())
+            {
+                for (int i = 0; i < lineCount; i++)
+                {
+                    ITextSnapshotLine line = snapshot.GetLineFromLineNumber(i);
+
+                    int length = line.Length;
+
+                    if (length == 0)
+                    {
+                        continue;
+                    }
+
+                    string content = line.GetText();
+
+                    int pos = length - 1;
+                    while (IsWhiteSpace(content[pos]))
+                    {
+                        pos --;
+                    }
+
+                    if (pos == length - 1)
+                    {
+                        continue;
+                    }
+
+                    edit.Delete(line.Start.Position + pos + 1, length - 1 - pos);
+                }
+
+                edit.Apply();
             }
         }
 
